59d3894b09c24e6de857154345f3ad1768209f15
germanftorres <germanftorres@outlook.com>
2014-04-24 01:02:51 +0200

    Commit inicial

---
+++ WebGit1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebGit1.MvcApplication" Language="C#" %>
---
+++ WebGit1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebGit1.Startup))]
+namespace WebGit1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
